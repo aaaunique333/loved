b869f8239e0cbc645bc6d5648ffc88e3d810b909
vpotinda@gmail.com <vpotinda@gmail.com>
2022-01-31 23:34:44 +0530

    31 jan 23:34

--- frontend/src/Container/update/Update.js
+++ frontend/src/Container/update/Update.js
@@ -1,14 +1,52 @@
 import classes from './Update.module.css'
-import React from 'react'
 import { FaPlus } from 'react-icons/fa'
 import Button from '../../components/Layout/Button'
+import { useHistory, useParams } from 'react-router-dom'
+import { useEffect } from 'react'
+import axios from 'axios'
+import { useState } from 'react'
 
-const Update = () => {
+const Update = () => { 
+  const history=useHistory();
+  const {id}=useParams();
+  const [title ,setTitle]=useState();
+  const [desc ,setDesc]=useState();
+  const [category,setCategory]=useState();
+
+  const onUpdateHandler=async()=>{
+    
+    const res=await axios({
+      method:"PUT",
+      url:`/update/${id}`,
+      data:{
+        title,desc,category
+      }
+    });
+
+    
+    history.push('/dashboard');
+    console.log(res);
+
+  }
+
+  
+
+  useEffect(()=>{
+    const fetchData=async()=>{
+      const res=await axios.put(`/update/${id}`);
+      console.log(res.data);
+      setTitle(res.data.title);
+      setDesc(res.data.desc);
+      setCategory(res.data.category);   
+    }
+    fetchData();
+
+  },[]);
     return (
-        <form className={classes['create-post']}>
+        <form className={classes['create-post']} onSubmit={onUpdateHandler}>
             <div className={classes.title}>
                 <FaPlus className={classes['title-icon']} />
-                <input type="text" placeholder="Enter the title here ....."/>
+                <input value={title} onChange={e=>setTitle(e.target.value)} type="text" placeholder="Enter the title here ....."/>
             </div>
             <div className={classes.image}>
                 <img src="https://api.hub.jhu.edu/factory/sites/default/files/styles/soft_crop_2400/public/stock_market_031521.jpg?itok=FlOOokap" alt=""/>
@@ -16,33 +54,18 @@ const Update = () => {
             </div>
             <div className={classes.category}>
                 <label htmlFor="category">Category:</label>
-               <select>
-                   <option>Html</option>
-                   <option>Css</option>
-                   <option>JS</option>
+               <select value={category} onChange={e=>setCategory(e.target.value)}>
+                   <option value="HTML">Html</option>
+                   <option value="CSS">Css</option>
+                   <option value="JS">JS</option>
                </select>
             </div>
             <div className={classes.paragraph}>
               <FaPlus className={classes['title-icon']} />
-                <textarea rows="5" ></textarea>
+                <textarea value={desc} onChange={e=>setDesc(e.target.value)}  rows="5" ></textarea>
             </div>
-            <div className={classes.paragraph}>
-              <FaPlus className={classes['title-icon']} />
-                <textarea rows="5" ></textarea>
-            </div>
-            <div className={classes.paragraph}>
-              <FaPlus className={classes['title-icon']} />
-                <textarea rows="5" ></textarea>
-            </div>
-        
-            <div className={classes.paragraph}>
-              <FaPlus className={classes['title-icon']} />
-                <textarea rows="5" ></textarea>
-            </div>
-            <div className={classes.paragraph}>
-              <FaPlus className={classes['title-icon']} />
-                <textarea rows="5" ></textarea>
-            </div>
+            
+            
             <Button className={classes.publish}>Update</Button>
 
         </form>
